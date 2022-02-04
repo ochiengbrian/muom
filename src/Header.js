ffdc9b50562761c9914e59cc61777de0da00cbf1
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-02-05 02:44:32 +0300

    init

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css"
+
+function Header() {
+  return <div>
+            <div className='Header__component'>
+                <h2>o1ne MASTERPIECE</h2>
+            </div>
+        </div>;
+}
+
+export default Header;
