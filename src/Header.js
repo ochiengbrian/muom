625b42123331aa48536574b5f4b61d66cedb35f2
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-02-05 03:14:02 +0300

    headercolor

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import "./Header.css"
 function Header() {
   return <div>
             <div className='Header__component'>
-                <h2>o1ne MASTERPIECE</h2>
+                <h1>o1ne MASTERPIECE</h1>
             </div>
         </div>;
 }
