9bf407d88ceaaa4d0462e3329114d82531ec6010
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-02-10 23:50:53 +0300

    header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return <div>
             <div className='Header'>
                 <div className='Header__component'>
-                    <h1>o1ne MASTERPIECE</h1>   
+                <h5>o1ne</h5> 
+                    <h2>MASTERPIECE</h2>  
+                    <p>Have you found what you were looking for?</p> 
                 </div>
             </div>
-        </div>;
+        </div>
 }
 
 export default Header;
