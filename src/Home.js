a9d4b318abba90215e8b7b7e69c6f363037022c0
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-02-15 02:42:05 +0300

    cards

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import react from 'react';
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
+      Some sample text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
