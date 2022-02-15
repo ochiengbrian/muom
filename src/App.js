4c52febe07f433504c0153fd41f60b3ba3375a25
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-02-15 13:27:28 +0300

    images and home cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Landing from './Landing';
 import Navbar from './Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
           <Route path='/navbar'>
             <Navbar />
+            <Home />
           </Route>
           <Route path='/'>
             <Landing />
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,22 @@
-import react from 'react';
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some sample text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+import React from 'react';
+import * as ReactBootStrap from 'react-bootstrap';
+import './Home.css'
+
+function Home() {
+  return (
+    <ReactBootStrap.Card style={{ width: '18rem' }}>
+    <ReactBootStrap.Card.Img variant="top" src="images/model-3.jpg"/>
+    <ReactBootStrap.Card.Body>
+      <ReactBootStrap.Card.Title>Tesla model 3</ReactBootStrap.Card.Title>
+      <ReactBootStrap.Card.Text>
+        Some sample text to build on the card title and make up the bulk of
+        the card's content.
+      </ReactBootStrap.Card.Text>
+      <ReactBootStrap.Button variant="primary">ADD TO CART</ReactBootStrap.Button>
+    </ReactBootStrap.Card.Body>
+  </ReactBootStrap.Card>
+  );
+  
+}
+
+export default Home;
